0d9a601650193ddd4e196469004e6957ae715fc5
sagar khursale <sagar.khursale@webmatrixtech.com>
2018-08-25 16:27:37 +0530

    Editor inputs and setupSpinner()

--- app/src/main/java/com/sagar/petstore/EditorActivity.java
+++ app/src/main/java/com/sagar/petstore/EditorActivity.java
@@ -3,18 +3,46 @@ package com.sagar.petstore;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
+
 public class EditorActivity extends AppCompatActivity {
+    /*Views*/
+    private EditText mNameEditText;
+    private EditText mBreedEditText;
+    private EditText mWeightEditText;
+    private Spinner mGenderSpinner;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editor);
 
-    // end
+        mNameEditText = findViewById(R.id.edit_pet_name);
+        mBreedEditText = findViewById(R.id.edit_pet_breed);
+        mWeightEditText = findViewById(R.id.edit_pet_weight);
+        mGenderSpinner = findViewById(R.id.spinner_gender);
+
+        // end
+    }
+
+    /* Drop-down for gender ------------------------*/
+    private void setupSpinner() {
+        ArrayAdapter genderSpinnerAdapter = ArrayAdapter.createFromResource(this,
+                R.array.array_gender_options, android.R.layout.simple_spinner_item);
+
+        genderSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+
+        mGenderSpinner.setAdapter(genderSpinnerAdapter);
     }
 
 
