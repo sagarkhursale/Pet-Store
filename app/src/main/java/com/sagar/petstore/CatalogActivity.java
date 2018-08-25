4b35e3667f11a8a26c1e63be88eb5ca32da84336
sagar khursale <sagar.khursale@webmatrixtech.com>
2018-08-25 15:20:07 +0530

    ListView with empty view

--- app/src/main/java/com/sagar/petstore/CatalogActivity.java
+++ app/src/main/java/com/sagar/petstore/CatalogActivity.java
@@ -8,9 +8,14 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
+
 
 
 public class CatalogActivity extends AppCompatActivity {
+
+    private ListView mListView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +32,11 @@ public class CatalogActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        mListView = findViewById(R.id.list_view_pet);
+
+        View emptyView = findViewById(R.id.empty_view);
+        mListView.setEmptyView(emptyView);
 
         // end
     }
