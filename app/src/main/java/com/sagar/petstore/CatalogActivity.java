2a4f95199d9e1ff9703c5a5c75ecbf366d72f65b
sagar khursale <sagar.khursale@webmatrixtech.com>
2018-08-25 19:25:17 +0530

    Loader callback initialized

--- app/src/main/java/com/sagar/petstore/CatalogActivity.java
+++ app/src/main/java/com/sagar/petstore/CatalogActivity.java
@@ -2,10 +2,15 @@ package com.sagar.petstore;
 
 import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -16,7 +21,9 @@ import android.widget.ListView;
 import com.sagar.petstore.data.PetContract;
 
 
-public class CatalogActivity extends AppCompatActivity {
+public class CatalogActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor>{
+
+    private static final int PET_LOADER = 0;
 
     private ListView mListView;
 
@@ -44,6 +51,9 @@ public class CatalogActivity extends AppCompatActivity {
 
         View emptyView = findViewById(R.id.empty_view);
         mListView.setEmptyView(emptyView);
+
+        // kick Off the loader.
+        getSupportLoaderManager().initLoader(PET_LOADER, null, this);
 
         // end
     }
@@ -89,6 +99,23 @@ public class CatalogActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    @NonNull
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, @Nullable Bundle args) {
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<Cursor> loader, Cursor data) {
+
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<Cursor> loader) {
+
     }
 
 
